02fb3a64f9ab81253b45516f832b0eee217faea0
Romain Graillot <graillot.romain@gmail.com>
2021-08-11 01:58:27 +0200

    Add a Date converter utility with strict parsing

--- app/src/main/java/com/notnotme/brewdogdiy/util/StringKt.kt
+++ app/src/main/java/com/notnotme/brewdogdiy/util/StringKt.kt
@@ -1,17 +1,36 @@
 package com.notnotme.brewdogdiy.util
 
+import org.apache.commons.validator.GenericValidator
 import java.text.SimpleDateFormat
 import java.util.*
 
 object StringKt {
 
-    val commonPatterns = listOf("MM-yyyy", "yyyy")
+    private val acceptPatterns by lazy {
+        listOf("MM/yyyy", "yyyy")
+    }
 
-    fun String.parse(patterns: List<String>): Date? {
-        for (pattern in patterns) {
-            try {
-                return SimpleDateFormat(pattern, Locale.US).parse(this)
-            } catch (_: Exception) { }
+    private val formatter by lazy {
+        SimpleDateFormat("", Locale.US).apply {
+            isLenient = true
+        }
+    }
+
+    /**
+     * Try to convert a String to a Date.
+     * Only a small subset of patterns are tested against the String.
+     * @see acceptPatterns
+     * @return null in case of failure, true otherwise
+     */
+    fun String.toDate(): Date? {
+        for (pattern in acceptPatterns) {
+            if (GenericValidator.isDate(this, pattern, true)) {
+                try {
+                    formatter.applyPattern(pattern)
+                    return formatter.parse(this)
+                } catch (_: Exception) {
+                }
+            }
         }
 
         return null
--- app/src/test/java/com/notnotme/brewdogdiy/StringKtTest.kt
+++ app/src/test/java/com/notnotme/brewdogdiy/StringKtTest.kt
@@ -1,24 +1,21 @@
 package com.notnotme.brewdogdiy
 
-import com.notnotme.brewdogdiy.util.StringKt
-import com.notnotme.brewdogdiy.util.StringKt.parse
+import com.notnotme.brewdogdiy.util.StringKt.toDate
 import org.junit.Assert.*
 import org.junit.Test
 import java.util.*
 
 class StringKtTest {
 
-    private val datePatterns = StringKt.commonPatterns
-
     @Test
     fun test_parse_string_fail() {
-        val date = "fail".parse(datePatterns)
+        val date = "fail".toDate()
         assertNull(date)
     }
 
     @Test
     fun test_parse_string_yyyy() {
-        val date = "2018".parse(datePatterns)
+        val date = "2018".toDate()
         assertNotNull(date)
 
         val cal = Calendar.getInstance()
@@ -27,13 +24,20 @@ class StringKtTest {
     }
 
     @Test
-    fun test_parse_string_MM_yyyy() {
-        val date = "04-2021".parse(datePatterns)
+    fun test_parse_string_MMyyyy() {
+        val date = "04/2021".toDate()
         assertNotNull(date)
 
         val cal = Calendar.getInstance()
         cal.time = date!!
         assertEquals(cal.get(Calendar.YEAR), 2021)
+        assertEquals(cal.get(Calendar.MONTH), Calendar.APRIL)
+    }
+
+    @Test
+    fun test_parse_string_MM_yyyy() {
+        val date = "04-2021".toDate()
+        assertNull(date)
     }
 
 }
